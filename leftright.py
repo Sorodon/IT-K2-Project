3dd12bda08f4898a9a0d9cc8a2680c89be628f61
Sorodon <Sorodon@gmx.net>
2017-02-21 16:18:08 +0100

    Create leftright.py

---
+++ leftright.py
@@ -0,0 +1,10 @@
+#leftright.py
+#Functions to move the Brick (or rather its field) left and right.
+
+def left(ymin, xmin, currbrickstate):
+	for y in range(1, 5):
+		for x in range(1, 5):
+			world2[ymin + (
+  
+ def right(ymin, ymax, currbrickstate):
+  
