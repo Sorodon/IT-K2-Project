2050380fea89a44362d2a45fd99c15e8ce5661b0
Sorodon <Sorodon@gmx.net>
2017-02-23 12:16:47 +0100

    Update falling.py

--- falling.py
+++ falling.py
@@ -1,11 +1,12 @@
 #will control the the falling Brick (= moving it down one by one)
 
-def fall(ymin, xmin, curbrickstate):
+def fall(ymin, xmin, currbrickstate):
 #arguments: 
 #	ymin, xmin: the coordinates of the upper corner of the Brickfield.
 #	curbrickstate: the rotation state of the brick that is used right now.
 
 	for y in range(1, 5):
-			for x in range(1, 5):
-    			world2[ymin + y][xmin + x] = curbrickstate[y][x]
+		for x in range(1, 5):
+    			world2[ymin + y][xmin + x] = currbrickstate[y][x]
 #	replaces the current brickfield with the brickfield given with curbrickstate first horizontally, then vertically
+	world2[ymin] = [0, 0, 0, 0]
