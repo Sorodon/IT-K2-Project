1e98b919522363d4d2a6a6b8d125bfc644f1dcbf
Sorodon <Sorodon@gmx.net>
2017-02-23 12:12:55 +0100

    Update leftright.py

--- leftright.py
+++ leftright.py
@@ -2,9 +2,11 @@
 #Functions to move the Brick (or rather its field) left and right.
 
 def left(ymin, xmin, currbrickstate):
-	for y in range(1, 5):
-		for x in range(1, 5):
-			world2[ymin + (
-  
- def right(ymin, ymax, currbrickstate):
-  
+	for y in range(0, 4):
+		for x in range(0, 4)
+		world2[ymin + y][xmin + (x - 1)] = currbrickstate[y][x]
+		
+def right(ymin, ymax, currbrickstate):
+	for y in range(0, 4):
+		for x in range(0, 4):
+			world2[ymin + y][xmin + (x + 1)] = currbrickstate[y][x]
